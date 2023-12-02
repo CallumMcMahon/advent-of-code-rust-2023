f072de8bb6f91ac5f0713a733553f556aba984d4
Callum McMahon <callum.a.mcmahon@gsk.com>
2023-12-02 02:17:02 +0000

    clean up template

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+advent_of_code::solution!(1);
+
 struct Calibration {
     first: char,
     last: char,
@@ -116,25 +118,19 @@ pub fn part_two(input: &str) -> Option<u32> {
     return Some(count);
 }
 
-fn main() {
-    let input = &advent_of_code::read_file("inputs", 1);
-    advent_of_code::solve!(1, part_one, input);
-    advent_of_code::solve!(2, part_two, input);
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_part_one() {
-        let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_one(&input), Some(142));
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(142));
     }
 
     #[test]
     fn test_part_two() {
-        let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_two(&input), Some(281));
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(281));
     }
 }
