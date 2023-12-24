bff90737685dc135e4c9448faa7855a72cbb9a89
Callum McMahon <callum.a.mcmahon@gsk.com>
2023-12-24 14:10:03 +0100

    day 7 part 2

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -14,11 +14,11 @@ mod input_parser {
     };
 
     fn parse_hand(input: &str) -> IResult<&str, Hand> {
-        let (input, cards) = take(5usize)(input)?;
+        let (input, card_str) = take(5usize)(input)?;
         let (input, _) = tag(" ")(input)?;
         let (input, bet) = character::complete::u32(input)?;
         let (input, _) = multispace0(input)?;
-        Ok((input, Hand::from(cards, bet).unwrap()))
+        Ok((input, Hand::from(card_str, bet).unwrap()))
     }
 
     pub fn parse_input(input: &str) -> Result<Vec<Hand>, NomError<&str>> {
@@ -29,6 +29,7 @@ mod input_parser {
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
 enum Card {
+    Joker,
     Two,
     Three,
     Four,
@@ -74,6 +75,20 @@ enum HandType {
     FourKind,
     FiveKind,
 }
+impl HandType {
+    fn from_counts(ordered_counts: &[i32]) -> Result<Self, &str> {
+        match ordered_counts {
+            [5] => Ok(HandType::FiveKind),
+            [1, 4] => Ok(HandType::FourKind),
+            [2, 3] => Ok(HandType::FullHouse),
+            [1, 1, 3] => Ok(HandType::ThreeKind),
+            [1, 2, 2] => Ok(HandType::TwoPair),
+            [1, 1, 1, 2] => Ok(HandType::OnePair),
+            [1, 1, 1, 1, 1] => Ok(HandType::HighCard),
+            _ => Err("Unexpected number of cards"),
+        }
+    }
+}
 
 #[derive(Debug, Eq, PartialEq, PartialOrd, Ord)]
 pub struct Hand {
@@ -91,24 +106,55 @@ impl Hand {
                 .and_modify(|count| *count += 1)
                 .or_insert(1);
         }
-        let mut ordered_counts = counts.values().collect::<Vec<_>>();
+        let mut ordered_counts = counts.values().cloned().collect::<Vec<_>>();
         ordered_counts.sort();
-        let hand_type;
-        match ordered_counts.as_slice() {
-            [5] => hand_type = Some(HandType::FiveKind),
-            [1, 4] => hand_type = Some(HandType::FourKind),
-            [2, 3] => hand_type = Some(HandType::FullHouse),
-            [1, 1, 3] => hand_type = Some(HandType::ThreeKind),
-            [1, 2, 2] => hand_type = Some(HandType::TwoPair),
-            [1, 1, 1, 2] => hand_type = Some(HandType::OnePair),
-            [1, 1, 1, 1, 1] => hand_type = Some(HandType::HighCard),
-            _ => hand_type = None,
-        }
+
+        let hand_type = HandType::from_counts(ordered_counts.as_slice()).unwrap();
         return Ok(Self {
-            hand_type: hand_type.unwrap(),
+            hand_type,
             cards,
             bet,
         });
+    }
+
+    /// transforms jacks into jokers, and applies joker hand_type rules. Does not
+    /// mutate self
+    fn joker_rule(&self) -> Self {
+        let cards: Vec<_> = self
+            .cards
+            .clone()
+            .into_iter()
+            .map(|card| {
+                if card == Card::Jack {
+                    Card::Joker
+                } else {
+                    card
+                }
+            })
+            .collect();
+        let mut counts = HashMap::new();
+        for card in cards.iter() {
+            counts
+                .entry(*card)
+                .and_modify(|count| *count += 1)
+                .or_insert(1);
+        }
+        let joker_count = counts.remove(&Card::Joker).unwrap_or(0);
+        let mut ordered_counts = counts.values().cloned().collect::<Vec<_>>();
+        ordered_counts.sort();
+        if ordered_counts.is_empty() {
+            // hand made of 5 jokers/jacks
+            ordered_counts = vec![joker_count]
+        } else {
+            *ordered_counts.last_mut().unwrap() += joker_count;
+        }
+
+        let hand_type = HandType::from_counts(ordered_counts.as_slice()).unwrap();
+        return Self {
+            hand_type,
+            cards,
+            bet: self.bet,
+        };
     }
 }
 
@@ -124,8 +170,17 @@ pub fn part_one(input: &str) -> Option<u32> {
     )
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let hands = input_parser::parse_input(input).unwrap();
+    let mut hands: Vec<_> = hands.iter().map(|hand| hand.joker_rule()).collect();
+    hands.sort();
+    Some(
+        hands
+            .iter()
+            .enumerate()
+            .map(|(i, hand)| (i + 1) as u32 * hand.bet)
+            .sum(),
+    )
 }
 
 #[cfg(test)]
@@ -141,6 +196,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(5905));
     }
 }
